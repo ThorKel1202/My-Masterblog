9ffa70be1584e32014ab6e2a490765d55b269509
Thorsten Kelterborn <tk@friendsandeye.de>
2026-07-31 13:03:33 +0200

    Modified app.py by creating new route add and created new add.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 
@@ -13,6 +13,14 @@ def load_posts():
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
 
 
 if __name__ == '__main__':

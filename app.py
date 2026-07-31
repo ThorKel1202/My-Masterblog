0f29694750919f6a9fad8fb37ee16dd08d2b0563
Thorsten Kelterborn <tk@friendsandeye.de>
2026-07-31 18:55:13 +0200

    Add the docstrings to app.py

--- app.py
+++ app.py
@@ -6,16 +6,29 @@ app = Flask(__name__)
 
 
 def load_posts():
+    """
+        Help-function to load and read the JSON file and return a list of dictionaries of all posts
+    """
+    
     with open("blog_data.json", "r", encoding="utf-8") as file:
         return json.load(file)
     
 
 def save_posts(posts):
+    """
+        Help-function that gets the new or updated data for the JSON file and writes the new JSON file
+    """
+    
     with open("blog_data.json", "w", encoding="utf-8") as file:
         json.dump(posts, file, indent=4, ensure_ascii=False)
         
         
 def fetch_post_by_id(post_id):
+    """
+        Help-function loads the current data from help-function load_posts and search for a given post_id.
+        After that it returns the corresponding post or None, if no corresponding post was found.
+    """
+    
     posts = load_posts()
     for post in posts:
         if post["id"] == post_id:
@@ -25,12 +38,24 @@ def fetch_post_by_id(post_id):
 
 @app.route('/')
 def index():
+    """
+        Function loads a list of dictionaries of all posts via help-function load_posts,
+        then returns impulse to render jinja2 template via flask which shows all existing posts.
+    """
+    
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """
+        Function calls a form to add new post in add.html and takes the new data. Then loads a list of dictionaries
+        of all posts via help-function load_posts, then sets a new ID and appends all new data to the existing posts.
+        After that it gives the new data to the help-function save_posts to update the JSON file.
+        Finally, it returns to the index page.
+    """
+    
     if request.method == 'POST':
         # Takes new data from form
         author = request.form.get('author')
@@ -67,6 +92,13 @@ def add():
 
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
+    """
+        Function loads a list of dictionaries of all posts via help-function load_posts,
+        with list comprehension it compares all posts which have not the given post_id and
+        copies them to a new list (of dictionaries). After that it gives the new data to the
+        help-function save_posts to update the JSON file. Finally, it returns to the updated index page.
+    """
+    
     # Load existing posts from JSON file via load_posts function
     posts = load_posts()
     
@@ -82,6 +114,14 @@ def delete(post_id):
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
+    """
+        Function loads a list of dictionaries of all posts via help-function load_posts,
+        and loads a single post of the given post_id from the helper-function fetch_post_by_id.
+        It renders the update.html with filled text fields for update them by the user. After that
+        it gives the new data to the help-function save_posts to update the JSON file.
+        Finally, it returns to the updated index page.
+    """
+    
     posts = load_posts()
     post = fetch_post_by_id(post_id)
 
@@ -114,6 +154,13 @@ def update(post_id):
 
 @app.route('/like/<int:post_id>')
 def like(post_id):
+    """
+        Function loads a list of dictionaries of all posts via help-function load_posts,
+        search for the given post_id and increases the likes counter by one.
+        After that it gives the new data to the help-function save_posts to update the JSON file.
+        Finally, it returns to the updated index page.
+    """
+    
     posts = load_posts()
 
     for post in posts:

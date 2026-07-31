488621ef5eb4b4e49fc54c0c79563207c8e3b9a0
Thorsten Kelterborn <tk@friendsandeye.de>
2026-07-31 11:25:33 +0200

    Modified app.route in app.py, created JSON file, copied style.css and copied and modified index.html

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+
 
 app = Flask(__name__)
 
 
+def load_posts():
+    with open("blog_data.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
